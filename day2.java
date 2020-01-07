10dab67cf9bb6080707b63517831ff6bb7b347e6
ShuoZheLi <37442704+ShuoZheLi@users.noreply.github.com>
2020-01-07 01:33:36 -0600

    Add files via upload

---
+++ day2.java
@@ -0,0 +1,22 @@
+﻿class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+        ListNode temp = head;
+        while (l1 != null && l2 != null) {
+            if (l2.val > l1.val) {
+                temp.next = l1;
+                l1 = l1.next;
+            } else {
+                temp.next = l2;
+                l2 = l2.next;
+            }
+            temp = temp.next;
+        }
+        if (l1 == null) {
+            temp.next = l2;
+        } else {
+            temp.next = l1;
+        }
+        return head.next;
+    }
+}
