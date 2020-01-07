a210b0c353f961ca835f41e3e08c38b3e111ea3e
ShuoZheLi <37442704+ShuoZheLi@users.noreply.github.com>
2020-01-07 01:34:57 -0600

    Update day2.java

--- day2.java
+++ day2.java
@@ -1,4 +1,7 @@
-﻿class Solution {
+//  想了很久如何在单个节点上操作，很多很多case但是过于复杂。
+//  还是没有看到问题的本质就是简单的对于，以及对剩余节点的处理。看了迭代的思路后恍然大悟。
+//  之后看到了递归的思路更想通了。后面又看到了用stack做，与前面迭代与递归相反，其本质就是比谁更小。
+class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         ListNode head = new ListNode(0);
         ListNode temp = head;
